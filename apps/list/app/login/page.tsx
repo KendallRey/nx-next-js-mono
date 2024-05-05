ab9b3e27015bf9712900f863456ec28149a139ff
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-05-05 19:46:09 +0800

    update login page, forum & list app

--- apps/list/app/login/page.tsx
+++ apps/list/app/login/page.tsx
@@ -1,5 +1,5 @@
 import styles from './page.module.scss';
-
+import { AuthComponent }from 'lib/ui/src'
 /* eslint-disable-next-line */
 export interface LoginProps {}
 
@@ -7,6 +7,7 @@ export default function Login(props: LoginProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Login!</h1>
+      <AuthComponent />
     </div>
   );
 }
