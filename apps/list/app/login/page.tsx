e02853c83a9a6f65d3bc49de1690f82bea78356d
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-05-05 18:53:08 +0800

    added login page for list app

---
+++ apps/list/app/login/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.scss';
+
+/* eslint-disable-next-line */
+export interface LoginProps {}
+
+export default function Login(props: LoginProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Login!</h1>
+    </div>
+  );
+}
