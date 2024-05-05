72a00597bd07dacfbfc606f548350b15b70c4ef6
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-05-05 20:15:17 +0800

    add sample login

--- lib/ui/src/lib/auth-component/auth-component.tsx
+++ lib/ui/src/lib/auth-component/auth-component.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './auth-component.module.scss';
+import { LoginRequestBody } from 'lib/login-api-types/src';
 
 /* eslint-disable-next-line */
 export interface AuthComponentProps {}
@@ -9,10 +10,27 @@ export function AuthComponent(props: AuthComponentProps) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Handle login logic here
+  const handleLogin = async () => {
+    const requestBody: LoginRequestBody = {
+      username,
+      password,
+    };
+    // call the login api using the inbuilt browser fetch library at http://localhost:3333/api/login with the username and password as the body
+    // if the response is 200, then redirect to the home page
+    // if the response is 401, then show an error message
+    const response = await fetch('http://localhost:3333/api/login', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (response.status === 200) {
+      window.location.href = '/';
+    } else {
+      alert('Invalid username or password');
+    }
   };
-
   return (
     <div className={styles['container']}>
       <h1>Login to Y Organization</h1>
