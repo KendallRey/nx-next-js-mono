926cf06348cea62c580a306a6461b1180dc4b989
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-05-05 19:35:04 +0800

    update auth-component

--- lib/ui/src/index.ts
+++ lib/ui/src/index.ts
@@ -1,3 +1,6 @@
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+'use client';
+
+export * from './lib/lib-ui';
+export * from './lib/auth-component/auth-component';
 
 export * from './lib/lib-ui';
--- lib/ui/src/lib/auth-component/auth-component.tsx
+++ lib/ui/src/lib/auth-component/auth-component.tsx
@@ -1,12 +1,38 @@
+import { useState } from 'react';
 import styles from './auth-component.module.scss';
 
 /* eslint-disable-next-line */
 export interface AuthComponentProps {}
 
 export function AuthComponent(props: AuthComponentProps) {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle login logic here
+  };
+
   return (
     <div className={styles['container']}>
-      <h1>Welcome to AuthComponent!</h1>
+      <h1>Login to Y Organization</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        id="username"
+        className={styles['input']}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        id="password"
+        className={styles['input']}
+      />
+      <button onClick={handleLogin} id="login-button" className={styles['button']}>Login</button>
     </div>
   );
 }
